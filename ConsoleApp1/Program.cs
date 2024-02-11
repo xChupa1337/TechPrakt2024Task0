be99bea89c953c86ffb33793149d47f6733b18f7
chupa_ipz223 <olegsokulskiy@gmail.com>
2024-02-11 20:29:20 +0200

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine($"Input a: ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine($"Input b: ");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine($"{a} + {b} = {a + b}");  
